7e2e3fed8593c64d2d6eb3d355c50f23d4241573
rajeshtr <rajeshtr@acube.co>
2014-03-25 19:39:01 +0530

    settings

--- modules/accounting/settings/view.php
+++ modules/accounting/settings/view.php
@@ -18,6 +18,13 @@ if(!defined('CHECK_INCLUDED')){
  			</div>
  			
  		</div>
+        <div class="row">
+ 			<div class="medium-5 columns">
+ 				<label for="ledger">Default Capital Ledger:</label>
+ 				<input type="text" />
+ 			</div>
+ 			
+ 		</div>
 
  		<div class="row">
  			<div class="medium-5 columns">
