75d19b7595b7414e2a8abfb08098a40ebfe6cd29
Rosy Swapna <rosy.swapna@acube.co>
2014-03-26 14:53:19 +0530

    add vazhipadu limit row

--- modules/vazhipadu/update/script.js
+++ modules/vazhipadu/update/script.js
@@ -15,8 +15,12 @@ $(document).ready(function(){
 
 		if(name!=''  && star_id >0){
 			var hiddenStr = name+'_'+star_id;
-					
-			$("#tbl-append").append('<tbody><tr><td>'+name+'<input type="hidden" name="hd_row[]" value="'+hiddenStr+'"></td><td>'+star+'<input type="hidden" name="hd_star[]" value="'+star+'"></td><td></td></tr></tbody>');
+			
+			$("#tbl-append").append('<tbody><tr><td>'+name+'<input type="hidden" class="hide-rows" name="hd_row[]" value="'+hiddenStr+'"></td><td>'+star+'<input type="hidden" name="hd_star[]" value="'+star+'"></td><td></td></tr></tbody>');
+			var row = $('.hide-rows').length;
+			if(row == 4){
+				$(this).hide();
+			}
 
 			$("input:text[name=txtname]").val('');
 			$("#liststar").val(-1);
