ccc978822882e10d3db76ff0bf073f47c3d3a358
Ratheesh Vijayan <ratheeshvijayan@acube.co>
2014-03-28 10:50:10 +0530

    login

--- modules/user/sign_in/view.php
+++ modules/user/sign_in/view.php
@@ -4,16 +4,21 @@ if ( !defined('CHECK_INCLUDED') ){
 }?>
 
 <h3>Login</h3> 
-<fieldset>
-    
+<fieldset style="background-color:#FFFFFF;">
 <div class="row" >
-	<div class="medium-12 columns">
+<div class="medium-4 columns"><img src="../../images/ayyappa.png" alt="" name="" width="321" height="268" />
+</div>
+	<div class="medium-8 columns">
+    <br />
+<br /><br />
+
+
 		<form  id="ajax-contact-form" target="_self" method="post" action="<?php echo $current_url?>" name="frmlogin">
 		<div class="medium-12 columns ">
 						<div class="medium-5 columns ">
 							<label>User Name <small>*</small></label>
 							<input name="loginname"  type="text" required class="text " id="loginname"  title=""  value=""  >
-						 </div>
+		  </div>
 						<div class="medium-5 columns ">
 					
 						</div>
@@ -23,7 +28,7 @@ if ( !defined('CHECK_INCLUDED') ){
 						<div class="medium-5 columns ">
 							<label>Password <small>*</small></label>
 							<input name="passwd"  type="password" required class="text" id="passwd" >
-						 </div>
+		  </div>
 						<div class="medium-5 columns ">
 					
 						</div>
@@ -34,7 +39,7 @@ if ( !defined('CHECK_INCLUDED') ){
 							<input  value="<?php echo $capSIGNIN; ?>" type="submit" name="submit" class="small button" >
          					    <input name="h_id" value="" type="hidden"><input name="h_login" value="pass" type="hidden">
 								<a href="forgot_password.php" class="button-link">Forgot Password?</a>
-						 </div>
+		  </div>
 						<div class="medium-5 columns ">
 					
 						</div>
