e9a4176152fbbddcfcd2d6f16c5c140bcb47b225
Rosy Swapna <rosy.swapna@acube.co>
2014-03-31 17:28:08 +0530

    account settings table altered

---
+++ files/DB/account_settings_db_change.sql
@@ -0,0 +1,6 @@
+ALTER TABLE `account_settings` ADD `organization_name` TEXT NULL ,
+ADD `organization_address` TEXT NULL ,
+ADD `tax_payers_id_no` TEXT NULL ,
+ADD `central_sales_tax_reg_no` TEXT NULL ,
+ADD `central_exise_reg_no` TEXT NULL ,
+ADD `reg_no` TEXT NOT NULL ;
