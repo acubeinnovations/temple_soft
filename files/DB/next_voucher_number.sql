247e289a19b4d0db62c8ccc3fef4f63fd828a2d1
Rosy Swapna <rosy.swapna@acube.co>
2014-05-28 10:35:57 +0530

    next voucher number function modified

--- files/DB/next_voucher_number.sql
+++ files/DB/next_voucher_number.sql
@@ -2,14 +2,13 @@
 
 DROP FUNCTION IF EXISTS GET_NEXT_VOUCHER_NUMBER;
 
-
 DELIMITER $$
 CREATE FUNCTION GET_NEXT_VOUCHER_NUMBER(voucher_id INT) RETURNS VARCHAR(11) DETERMINISTIC
 BEGIN
 DECLARE next_voucher_number VARCHAR(11);
 	SELECT IF (EXISTS ( SELECT 1 FROM account_master WHERE voucher_type_id = voucher_id),
-(SELECT MAX(voucher_number)+1 FROM account_master WHERE voucher_type_id = voucher_id) ,
-(SELECT DISTINCT  series_start FROM voucher WHERE voucher_id = voucher_id)) INTO next_voucher_number ;
+(SELECT MAX(CAST(voucher_number AS SIGNED))+1 FROM account_master WHERE voucher_type_id = voucher_id) ,
+(SELECT series_start FROM voucher WHERE voucher_id = voucher_id)) INTO next_voucher_number ;
 
 	RETURN next_voucher_number ;
 END$$
